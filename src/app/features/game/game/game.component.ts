f66f644f85c25e0c0cb192e2d4e3438be5e7d3a9
Nino Gachechiladze <ninogachechiladze@tbcbank.com.ge>
2022-12-04 15:23:04 +0400

    started working on game

--- src/app/features/game/game/game.component.ts
+++ src/app/features/game/game/game.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from 'src/app/interfaces/question';
+import { QuestionService } from 'src/app/shared/services/question.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-
-  constructor() { }
+  constructor(private questionService: QuestionService) {}
+  questions: Question[] = [];
+  currentQuestion: Question | undefined;
 
   ngOnInit(): void {
+    this.questionService
+      .getAllQuestions()
+      .subscribe((res) => (this.questions = res));
   }
-
 }
--- src/app/features/game/question/question.component.ts
+++ src/app/features/game/question/question.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Question } from 'src/app/interfaces/question';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  @Input() currentQuestion: Question | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
