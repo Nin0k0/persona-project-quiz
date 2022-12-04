5df18c0543e74b05e475949aea15f96da452728b
Nino Gachechiladze <ninogachechiladze@tbcbank.com.ge>
2022-12-04 22:30:37 +0400

    i am so sad

--- src/app/features/game/game.module.ts
+++ src/app/features/game/game.module.ts
@@ -4,9 +4,15 @@ import { QuestionComponent } from './question/question.component';
 import { GameComponent } from './game/game.component';
 import { GameRoutingModule } from './game-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [QuestionComponent, GameComponent],
-  imports: [CommonModule, GameRoutingModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    GameRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
 })
 export class GameModule {}
--- src/app/features/game/game/game.component.ts
+++ src/app/features/game/game/game.component.ts
@@ -1,5 +1,13 @@
 import { JsonPipe } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { Question } from 'src/app/interfaces/question';
 import { QuestionService } from 'src/app/shared/services/question.service';
 
@@ -9,14 +17,28 @@ import { QuestionService } from 'src/app/shared/services/question.service';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  constructor(private questionService: QuestionService) {}
+  skip() {
+    console.log(this.wordForm.value);
+  }
+
+  constructor(
+    private questionService: QuestionService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
   QuestionList: Question[] | undefined;
   currentQuestionIndex: number = 2;
   currentAnswerArr: string[] = [];
   currentAnswer: string = '';
   isGameStarted = false;
+  wordForm = this.formBuilder.group({
+    letters: this.formBuilder.array([]),
+  });
+
+  mytrying: any[] = [];
   ngOnInit(): void {
     this.getAllQs();
+    this.mytrying = [];
   }
 
   getAllQs() {
@@ -31,5 +53,9 @@ export class GameComponent implements OnInit {
     for (let letter of this.currentAnswer) {
       this.currentAnswerArr.push(letter);
     }
+  }
+
+  written(str: string, index: number) {
+    this.mytrying.push({ ind: index, value: str });
   }
 }
