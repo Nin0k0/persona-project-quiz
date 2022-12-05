8fb349955f0beef0f447d517205096aab4bbd5fb
Nino Gachechiladze <ninogachechiladze@tbcbank.com.ge>
2022-12-05 22:05:54 +0400

    game ukve ragacas gavs

--- src/app/features/game/game/game.component.ts
+++ src/app/features/game/game/game.component.ts
@@ -1,5 +1,11 @@
 import { JsonPipe } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  QueryList,
+} from '@angular/core';
 import {
   FormArray,
   FormBuilder,
@@ -8,6 +14,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { map, timeInterval } from 'rxjs';
 import { Question } from 'src/app/interfaces/question';
 import { QuestionService } from 'src/app/shared/services/question.service';
 
@@ -17,45 +24,68 @@ import { QuestionService } from 'src/app/shared/services/question.service';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  skip() {
-    console.log(this.wordForm.value);
-  }
-
-  constructor(
-    private questionService: QuestionService,
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
-  QuestionList: Question[] | undefined;
-  currentQuestionIndex: number = 2;
+  constructor(private questionService: QuestionService) {}
+  QuestionList!: Question[];
+  currentQuestionIndex: number = 0;
   currentAnswerArr: string[] = [];
   currentAnswer: string = '';
   isGameStarted = false;
-  wordForm = this.formBuilder.group({
-    letters: this.formBuilder.array([]),
-  });
+  score: number = 0;
+  formArr = new FormArray<FormControl<string>>([]);
 
-  mytrying: any[] = [];
   ngOnInit(): void {
     this.getAllQs();
-    this.mytrying = [];
   }
 
   getAllQs() {
-    this.questionService.getAllQuestions().subscribe((response) => {
+    this.questionService.getAllQuestions()!.subscribe((response) => {
       this.QuestionList = response;
+      this.other();
     });
   }
 
-  Play() {
-    this.isGameStarted = true;
-    this.currentAnswer = this.QuestionList![this.currentQuestionIndex].answer;
-    for (let letter of this.currentAnswer) {
-      this.currentAnswerArr.push(letter);
-    }
+  other() {
+    this.getWordFunc();
+    this.formArr.valueChanges
+      .pipe(
+        map((data) => {
+          if (data.length > 0) {
+            return data.reduce((a, b) => a + b);
+          }
+          return null;
+        })
+      )
+      .subscribe((data) => {
+        if (
+          data?.toLocaleLowerCase() == this.currentAnswer.toLocaleLowerCase()
+        ) {
+          //დავამატო ქულა
+          this.score += 1;
+
+          this.currentQuestionIndex += 1;
+          if (this.currentQuestionIndex > this.currentAnswer.length) {
+            //gadavide sxva mode-ze da morches kitxvebi
+          }
+          while (this.formArr.length !== 0) {
+            this.formArr.removeAt(0);
+          }
+          this.getWordFunc();
+        }
+      });
   }
 
-  written(str: string, index: number) {
-    this.mytrying.push({ ind: index, value: str });
+  getWordFunc() {
+    this.currentAnswer = this.QuestionList![this.currentQuestionIndex]!.answer;
+    this.currentAnswerArr =
+      this.QuestionList![this.currentQuestionIndex]!.answer.split('');
+    this.currentAnswerArr.forEach(() => {
+      const fcontrol = new FormControl('', { nonNullable: true });
+      fcontrol.valueChanges.subscribe((val) => {
+        if (val.length == 2) {
+          fcontrol.setValue(val[1]);
+        }
+      });
+      this.formArr.push(fcontrol);
+    });
   }
 }
